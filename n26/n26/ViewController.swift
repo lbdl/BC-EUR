12c7c3c604eff5f49c74909d51f9885ab1adaf82
Tim Storey <timstorey@bite-software.com>
2018-05-29 13:36:53 +0100

    Add another gitignore and tidy

---
+++ n26/n26/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  n26
+//
+//  Created by Timothy Storey on 29/05/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
